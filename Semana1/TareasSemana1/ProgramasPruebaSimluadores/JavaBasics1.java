bf70fbb36b1ea21745c082c7edae29c1557225d4
Johan Bravo <158103844+JohanBravoL@users.noreply.github.com>
2024-01-30 12:16:15 -0600

    prueba1

---
+++ Semana1/TareasSemana1/ProgramasPruebaSimluadores/JavaBasics1.java
@@ -0,0 +1,10 @@
+package Semana1.TareasSemana1.ProgramasPruebaSimluadores;
+
+public class JavaBasics1 {
+
+    final public static void main(String[] args) {
+        
+        System.out.println("Hola");
+    }
+    
+}
