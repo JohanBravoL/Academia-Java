4963cfeb230f7cafba4fc373d65f0c60c82be251
Johan Bravo <158103844+JohanBravoL@users.noreply.github.com>
2024-01-29 18:28:13 -0600

    Creacion de carpeta para tareas de la semana 1

---
+++ Semana1/TareasSemana1/Descripcion.java
@@ -0,0 +1,9 @@
+package Semana1.TareasSemana1;
+
+public class Descripcion 
+{
+    public static void main(String[] args)
+    {
+    System.out.println("La presente carpeta fungira para subir las tareas en torno a la semana 1");
+    }
+}
