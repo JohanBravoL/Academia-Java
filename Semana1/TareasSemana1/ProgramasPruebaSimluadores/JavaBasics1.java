87d231064e73a120791ad5aee91b96155c6ad772
Johan Bravo <158103844+JohanBravoL@users.noreply.github.com>
2024-01-30 12:47:59 -0600

    Prueba3

--- Semana1/TareasSemana1/ProgramasPruebaSimluadores/JavaBasics1.java
+++ Semana1/TareasSemana1/ProgramasPruebaSimluadores/JavaBasics1.java
@@ -2,6 +2,7 @@ package Semana1.TareasSemana1.ProgramasPruebaSimluadores;
 
 public class JavaBasics1 {
 
+        //Se puede utilizar el "final" antes sin embargo no es frecuentemente utilizado
     final public static void main(String[] args)
     {
         String x = "Hola";
