d6dbf15bae973324bb8ff286fc1b3e1786990a90
Johan Bravo <158103844+JohanBravoL@users.noreply.github.com>
2024-01-30 13:02:44 -0600

    ExplicacionEquals

---
+++ Semana1/ProgramaRealizados/Equals.java
@@ -0,0 +1,21 @@
+package Semana1.ProgramaRealizados;
+
+public class Equals 
+{
+    public static void main(String[] args) 
+    {
+        String s1 = "Hola";
+        String s2 = new String("Hola");
+
+            //El equals busca que sea el mismo contenido dentro del objeto
+        System.out.println(s1.equals(s2));
+        
+        Boolean z;
+
+        z = s1 == s2;
+
+        System.out.println(z);
+
+    }
+    
+}
