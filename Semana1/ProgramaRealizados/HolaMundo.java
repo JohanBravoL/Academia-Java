9ee549150e07ebe965e06058e1de77a35a691c1d
Johan Bravo <158103844+JohanBravoL@users.noreply.github.com>
2024-01-29 18:20:55 -0600

    Programa realizado en clase

---
+++ Semana1/ProgramaRealizados/HolaMundo.java
@@ -0,0 +1,17 @@
+package Semana1.ProgramaRealizados;
+
+public class HolaMundo 
+{
+    public static void main(String[] args) 
+    {
+        String x = "Hola";
+        String y = " Mundo";
+        String z = y;
+        y = null;
+        System.out.println("z: " + z);
+        System.out.println("y: " + y);
+        x = x.concat(z);
+        System.out.println("x: " + x);
+    }
+    
+}
