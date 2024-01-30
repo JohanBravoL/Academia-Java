a8660055035dae51eaa2ba43267f30011e224aed
Johan Bravo <158103844+JohanBravoL@users.noreply.github.com>
2024-01-30 13:09:29 -0600

    Prueba3

--- Semana1/ProgramaRealizados/Equals.java
+++ Semana1/ProgramaRealizados/Equals.java
@@ -6,16 +6,14 @@ public class Equals
     {
         String s1 = "Hola";
         String s2 = new String("Hola");
-
             //El equals busca que sea el mismo contenido dentro del objeto
         System.out.println(s1.equals(s2));
-        
         Boolean z;
-
+            //El == lo que busca es que sea el mismo objetono importando el contenido
         z = s1 == s2;
-
         System.out.println(z);
-
+            //El @Override se refiere a una sobreescritura.
     }
     
 }
+
