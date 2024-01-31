0e3371566ccd693c2d494f4db4f0e27b9028af40
Johan Bravo <158103844+JohanBravoL@users.noreply.github.com>
2024-01-30 21:10:51 -0600

    Prueba3

--- Semana1/TareasSemana1/Singleton/Singleton.java
+++ Semana1/TareasSemana1/Singleton/Singleton.java
@@ -3,15 +3,15 @@ package Semana1.TareasSemana1.Singleton;
 public class Singleton {
     public static void main(String[] args) {
         
-        carro cont1 = new carro("Sedan");
+        carro cont1 = carro.getInstance();
 
         cliente venta1 = new cliente("Volkswagen",cont1);
         cliente venta2 = new cliente("Chevrolet",cont1);
 
-        carro cont2 = new carro("Sedan");
+        carro cont2 = carro.getInstance();
         cliente venta3 = new cliente("BMW", cont2);
 
         System.out.println(venta1.cont1 == venta3.cont1);
-
+        System.out.println(carro.c1);
     }
 }
--- Semana1/TareasSemana1/Singleton/carro.java
+++ Semana1/TareasSemana1/Singleton/carro.java
@@ -1,13 +1,19 @@
 package Semana1.TareasSemana1.Singleton;
-
+// En esta clase se aplicara el Singleton
 public class carro {
 
     String bd;
     static int c1;
-
-    public carro(String bd){
+        //Primero se tiene que crear un objeto que es del mismo tipo de forma privada que sea estatica
+    static private carro cont1;
+        //Depues se tener metodo privado
+    private carro(String bd){
         this.bd = bd;
         c1++;
+    }
+        //Se debe de crear un metodo Getinstance que regresara el Singleton de forma estatica
+    static public carro getInstance(){
+        return cont1;
     }
     
 }
