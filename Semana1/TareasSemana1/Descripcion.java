b8847688fdb744923e62d9e5fd9764cefd6f65c2
Johan Bravo <158103844+JohanBravoL@users.noreply.github.com>
2024-01-30 21:24:47 -0600

    Delete Semana1/TareasSemana1/Descripcion.java

--- Semana1/TareasSemana1/Descripcion.java
+++
@@ -1,9 +0,0 @@
-package Semana1.TareasSemana1;
-
-public class Descripcion 
-{
-    public static void main(String[] args)
-    {
-    System.out.println("La presente carpeta fungira para subir las tareas en torno a la semana 1");
-    }
-}
