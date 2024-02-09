ed1398168cf19b0e17bee66b517e3051fd420607
Johan Bravo <158103844+JohanBravoL@users.noreply.github.com>
2024-02-09 13:52:35 -0600

    Pruebas simuladores

---
+++ Semana2/ProgramasDeSesion/Simuladores/P4/Prueba4.java
@@ -0,0 +1,20 @@
+package Semana2.ProgramasDeSesion.Simuladores.P4;
+
+public class Prueba4 {
+    public static void main(String args[ ] ){       
+		int i = 0 ;       
+		boolean bool1 = true ;       
+		boolean bool2 = false;       
+		boolean bool  = false;       
+		bool = ( bool2 &  method1(i++) ); //1       
+		bool = ( bool2 && method1(i++) ); //2       
+		bool = ( bool1 |  method1(i++) ); //3       
+		bool = ( bool1 || method1(i++) ); //4    
+		
+		System.out.println(i);    
+	}    
+	
+	public static boolean method1(int i){        
+		return i>0 ? true : false;    
+	} 
+}
---
+++ Semana2/ProgramasDeSesion/Simuladores/P6/Prueba6.java
@@ -0,0 +1,10 @@
+package Semana2.ProgramasDeSesion.Simuladores.P6;
+
+public class Prueba6 {
+    public static void main(String[ ] args){       
+    int[] a = { 1, 2, 3, 4 };       
+    int[] b = { 2, 3, 1, 0 };       
+    System.out.println( a [ (a = b)[3] ] );
+    
+    }
+}
